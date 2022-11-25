a6b4fff69ebd0e550b206645ad7d6f09e9aba154
carlito1979 <carlitoandersson@gmail.com>
2022-11-25 23:01:45 +1300

    Update AntServer.js

--- app/ant/AntServer.js
+++ app/ant/AntServer.js
@@ -9,9 +9,11 @@
   - Garmin USB or USB2 ANT+ or an off-brand clone of it (ID 0x1008)
   - Garmin mini ANT+ (ID 0x1009)
 */
-import log from 'loglevel'
+import loglevel from 'loglevel'
 import Ant from 'gd-ant-plus'
 import {Timer} from './timer.js'
+
+const log = loglevel.getLogger('default')
 
 const DEVICE_TYPE = 0x11; // Ant FE-C device
 const DEVICE_NUMBER = 1;
@@ -112,17 +114,6 @@ export class AntServer {
     for (let m of messages) {
       stick.write(m);
     }
-  }
-
-  /**
-   * Update instantaneous power and cadence.
-   * @param {object} measurement
-   * @param {number} measurement.power - power in watts
-   * @param {number} measurement.cadence - cadence in rpm
-   */
-  updateMeasurement({ power, cadence }) {
-    this.power = power;
-    this.cadence = cadence;
   }
 
   /**
@@ -235,6 +226,8 @@ export class AntServer {
           0xFF, // heart rate not being sent
           ...Ant.Messages.intToLEHexArray(this.capabilitiesState, 1)
         ]
+        log.debug(`Page 16 Data Sent. Event=${this.eventCount}. Time=${this.accumulatedTime}. Distance=${this.accumulatedDistance}. Speed=${Math.round(this.totalLinearDistance *1000)}.`)
+        log.debug(`Hex Time=0x${this.accumulatedTime.toString(16)}. Hex Distance=0x${this.accumulatedDistance.toString(16)}. Hex Speed=0x${Math.round(this.totalLinearDistance *1000).toString(16)}.`)
         break
       case 17: // 0x11 - General Settings Page (once a second)
         data = [
@@ -247,6 +240,8 @@ export class AntServer {
           ...Ant.Messages.intToLEHexArray(this.dragFactor, 1),// Drag Factor Interpreted as Resistance. DF 50 = 0%, DF 250 = 100% (e.g., DF 130 = 40% resistance) each unit = 0.5%
           ...Ant.Messages.intToLEHexArray(this.capabilitiesState, 1)
         ]
+        log.debug(`Page 17 Data Sent. Event=${this.eventCount}. Stroke Length=${Math.round(this.driveLength * 100)}. Drag Factor=${this.dragFactor}.`)
+        log.debug(`Hex Stroke Length=0x${Math.round(this.driveLength * 100).toString(16)}. Hex Drag Factor=0x${this.dragFactor.toString(16)}.`)
         break
       case 22: // 0x16 - Specific Rower Data (once a second)
         data = [
@@ -259,6 +254,8 @@ export class AntServer {
           ...Ant.Messages.intToLEHexArray(this.cyclePower, 2), // Instant Power (2 bytes)
           ...Ant.Messages.intToLEHexArray(this.capabilitiesState, 1)
         ]
+        log.debug(`Page 22 Data Sent. Event=${this.eventCount}. Strokes=${this.accumulatedStrokes}. Stroke Rate=${this.cycleStrokeRate}. Power=${this.cyclePower}`)
+        log.debug(`Hex Strokes=0x${this.accumulatedStrokes.toString(16)}. Hex Stroke Rate=0x${this.cycleStrokeRate.toString(16)}. Hex Power=0x${this.cyclePower.toString(16)}`)
         break
       case 80: // 0x50 - Common Page for Manufacturers Identification (approx twice a minute)
         data = [
