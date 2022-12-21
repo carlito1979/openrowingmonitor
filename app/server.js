6d16fff1e224d1ab5479a85dbd6af56a0ee2b111
Abász <>
2022-12-21 13:01:58 +0100

    Add ANT+ profile manager
    - Implement an ANT+ profile manager similar to BLE
    - Implement ANT+ Fitness Equipment profile to be able to broadcast data
    - Add button to the UI to scroll through the ANT+ profiles

--- app/server.js
+++ app/server.js
@@ -88,6 +88,10 @@ peripheralManager.on('control', (event) => {
       webServer.notifyClients('config', getConfig())
       event.res = true
       break
+    case 'antPeripheralMode':
+      webServer.notifyClients('config', getConfig())
+      event.res = true
+      break
     case 'hrmPeripheralMode':
       webServer.notifyClients('config', getConfig())
       event.res = true
@@ -191,6 +195,9 @@ webServer.on('messageReceived', async (message, client) => {
     case 'switchBlePeripheralMode':
       peripheralManager.switchBlePeripheralMode()
       break
+    case 'switchAntPeripheralMode':
+      peripheralManager.switchAntPeripheralMode()
+      break
     case 'switchHrmMode':
       peripheralManager.switchHrmMode()
       break
@@ -219,6 +226,7 @@ webServer.on('clientConnected', (client) => {
 function getConfig () {
   return {
     blePeripheralMode: peripheralManager.getBlePeripheralMode(),
+    antPeripheralMode: peripheralManager.getAntPeripheralMode(),
     hrmPeripheralMode: peripheralManager.getHrmPeripheralMode(),
     stravaUploadEnabled: !!config.stravaClientId && !!config.stravaClientSecret,
     shutdownEnabled: !!config.shutdownCommand
